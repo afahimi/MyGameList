d6a8ecf57dd34e131363a518d5ca22e976d2c697
afahimi <amin.f1000@gmail.com>
2024-06-09 13:01:35 -0700

    init jwt verify method

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,25 @@ const client = new Client({
   database: process.env.DB_NAME,
 });
 
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+      const token = authHeader.split(' ')[1];
+
+      jwt.verify(token, SECRET_KEY, (err, user) => {
+          if (err) {
+              return res.sendStatus(403);
+          }
+
+          req.user = user;
+          next();
+      });
+  } else {
+      res.sendStatus(401);
+  }
+};
+
 await client.connect();
 app.use(cors());
 app.use(express.json());
